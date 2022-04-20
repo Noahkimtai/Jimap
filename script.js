749d3341a2dfbb19d3781b3a472acffad123a7eb
Noahkimtai <noahkimtai24@gmail.com>
2022-04-20 10:14:23 +0300

    accessing the edited layer shapes code added

--- script.js
+++ script.js
@@ -25,3 +25,39 @@ new L.Control.Draw({
 // new keyword is a way to create a new object in javascript
 // You can also create a new object by calling a constructor function eg. L.map, L.marker
 // L.control.Draw function accep an options boject where we can alter the structure and behavior of the drawing control
+
+//with the above code the shape disapear after editing
+// to make the shape persist, draw control fires events we can listen and respond to e.g. draw:created which has a property layer which has the shape created.
+// when the event fires we can capture the edited layer e.layer
+// add elayer to the drawnItems group
+// it is then editable
+map.adIventListener('draw:created', function(e){
+    e.layer.addTo(drawnItems);
+    drawnItems.eachLayer(function(layer){
+        let geoJson = JSON.stringify(layer.toGeoJSON().geometry)
+        console.log(geoJson);
+    });
+});
+
+/* save the  shapes stored in the drawnItems to a GeoJson file
+drawnItems.eachLayer(function(layer){
+    let geoJson = JSON.stringify(layer.toGeoJSON().geometry)
+    console.log(geoJson);
+}) */
+//.eachLayer method is used for applying something to each layer in the group object
+//it takes one parameter layer
+// it is similar to .foreach array method
+
+// set such that the user can edit only one shape at a time
+// this does the reverse of the previous code, instead of creating shapes and converting them to GEOJSON string
+new L.Control.Draw({
+    edit: false
+}).addTo(map)
+
+//to display GEOJSON of drawn shapes listen to event
+function showText(e){
+    layer.clearLayers();
+    let layer = e.layer
+    layer.addTo(layers);
+    let geoJson = JSON.stringify 
+}
